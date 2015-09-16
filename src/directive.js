aeee85996d4c1ddea5d72a314f4f969d3e63f1c8
Jared Lewis <jared@jared.kiwi.nz>
2015-09-16 13:07:03 +1200

    Converted ol.control.LayerSwitcher to a angular module
    The angular module is named openlayers-layerswitcher to align with openlayers-directive

---
+++ src/directive.js
@@ -0,0 +1,19 @@
+angular.module('openlayers-layerswitcher', ['openlayers-directive'])
+  .directive('layerSwitcher', function(olControlLayerSwitcher) {
+    return {
+      restrict: 'E',
+      require: '^openlayers',
+      link: function (scope, _, _, controller) {
+        var olControl = new olControlLayerSwitcher();
+        var olScope = controller.getOpenlayersScope();
+
+        olScope.getMap().then(function(map) {
+          scope.$on('$destroy', function() {
+            map.removeControl(olControl);
+          });
+
+          map.addControl(olControl);
+        });
+      }
+    };
+  });
